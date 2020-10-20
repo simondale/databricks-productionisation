940ad225cd2b978f2f47dff713426c173b5324b6
Simon Dale <simon.dale@bjss.com>
2020-10-20 22:17:44 +0100

    Black formatting applied

--- src/iris_model/log_analytics.py
+++ src/iris_model/log_analytics.py
@@ -17,7 +17,7 @@ class LogAnalytics:
         """
         self.id = uuid.uuid4()
 
-    def init(self, customer_id, shared_key, log_type, category=''):
+    def init(self, customer_id, shared_key, log_type, category=""):
         """Initialise this instance so that it is ready to send messages
            to a Log Analytics workspace.
 
@@ -40,17 +40,21 @@ class LogAnalytics:
             message:        The log message to sent to Log Analytics
         """
         if self.log_type is not None:
-            msg = [{
-                'id': str(uuid.uuid4()),
-                'tracker': str(self.id),
-                'level': level,
-                'category': self.category,
-                'message': message
-            }]
+            msg = [
+                {
+                    "id": str(uuid.uuid4()),
+                    "tracker": str(self.id),
+                    "level": level,
+                    "category": self.category,
+                    "message": message,
+                }
+            ]
             body = json.dumps(msg)
             self._post_data(body)
-    
-    def _build_signature(self, date, content_length, method, content_type, resource):
+
+    def _build_signature(
+        self, date, content_length, method, content_type, resource
+    ):
         """Create a signature for the specified message.
 
         Args:
@@ -63,12 +67,16 @@ class LogAnalytics:
         Returns:
             The `SharedKey` authorization header
         """
-        x_headers = f'x-ms-date:{date}'
-        string_to_hash = f'{method}\n{str(content_length)}\n{content_type}\n{x_headers}\n{resource}'
-        bytes_to_hash = string_to_hash.encode('utf-8')
+        x_headers = f"x-ms-date:{date}"
+        string_to_hash = f"{method}\n{str(content_length)}\n{content_type}\n{x_headers}\n{resource}"
+        bytes_to_hash = string_to_hash.encode("utf-8")
         decoded_key = base64.b64decode(self.shared_key)
-        encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest()).decode('utf-8')
-        authorization = f'SharedKey {self.customer_id}:{encoded_hash}'
+        encoded_hash = base64.b64encode(
+            hmac.new(
+                decoded_key, bytes_to_hash, digestmod=hashlib.sha256
+            ).digest()
+        ).decode("utf-8")
+        authorization = f"SharedKey {self.customer_id}:{encoded_hash}"
         return authorization
 
     def _post_data(self, body):
@@ -80,18 +88,22 @@ class LogAnalytics:
         Returns:
             The response from the remote server
         """
-        method = 'POST'
-        content_type = 'application/json'
-        resource = '/api/logs'
-        rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        method = "POST"
+        content_type = "application/json"
+        resource = "/api/logs"
+        rfc1123date = datetime.datetime.utcnow().strftime(
+            "%a, %d %b %Y %H:%M:%S GMT"
+        )
         content_length = len(body)
-        signature = self._build_signature(rfc1123date, content_length, method, content_type, resource)
-        uri = f'https://{self.customer_id}.ods.opinsights.azure.com{resource}?api-version=2016-04-01'
+        signature = self._build_signature(
+            rfc1123date, content_length, method, content_type, resource
+        )
+        uri = f"https://{self.customer_id}.ods.opinsights.azure.com{resource}?api-version=2016-04-01"
         headers = {
-            'Content-Type': content_type,
-            'Authorization': signature,
-            'Log-Type': self.log_type,
-            'x-ms-date': rfc1123date
+            "Content-Type": content_type,
+            "Authorization": signature,
+            "Log-Type": self.log_type,
+            "x-ms-date": rfc1123date,
         }
         response = requests.post(uri, data=body, headers=headers)
         return response
